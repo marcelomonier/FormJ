7693c47244b11b05deed1a68d5b64b1b9bd90a03
Jesus Cuesta <jesusdanielcf@gmail.com>
2016-05-08 13:14:02 +0200

    Adding Logger

--- src/form/FormController.java
+++ src/form/FormController.java
@@ -19,10 +19,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 
-/**
- *
- * @author Jesus Daniel Cuesta fuentes
- */
 public class FormController implements Initializable {
 
     private Label label;
